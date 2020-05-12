265dc417a678fc2ce46e91260d5eb638195eede6
Your Name <you@example.com>
2020-05-13 06:34:27 +1000

    Completed excercise steps till 3.16

--- backend/index.js
+++ backend/index.js
@@ -26,17 +26,9 @@ app.use(morgan(function (tokens, req, res) {
     return log;
   }))
 
-let persons = [ { name: 'Arto Hellas', number: '040-123456', id:1 },
-{ name: 'Ada Lovelace', number: '39-44-5323523', id:2 },
-{ name: 'Dan Abramov', number: '12-43-234345', id:3  },
-{ name: 'Mary Poppendieck', number: '39-23-6423122' , id:4}];
-
-const generateRandomId = () => Math.floor(Math.random() * 1000000);
-
-
 app.get('/api/persons', (request,response) => {
 Person.find({}).then(persons => {
- response.json(persons);
+ response.json(persons.map(person => person.toJSON()));
 })
 })
 
@@ -48,24 +40,21 @@ app.get('/info', (request, response) => {
     response.send(infoResponse)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id, function(err, person) {
-    if(err) {
-     return response.status(404).end();
+app.get('/api/persons/:id', (request, response,next) => {
+  Person.findById(request.params.id).then(person => {
+    if(person){
+      response.json(person.toJSON());
+    } else {
+      response.status(404).send({'error': 'Missing person'})
     }
-    response.json(person.toJSON())
-  })
+  }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndDelete(request.params.id, function(err){
-      if(err) {
-        return response.status(404).end()
-      }
-      response.status(204).end();
+app.delete('/api/persons/:id', (request, response,next) => {
+    Person.findByIdAndRemove(request.params.id).then(result => {
+      response.status(204).end()
+    }).catch(error => next(error))
     })
-    
-})
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -80,10 +69,38 @@ app.post('/api/persons', (request, response) => {
   })
 
   person.save().then(savedPerson => {
-    response.json(savedPerson.toJSON())
-    mongoose.connection.close()
+    response.json(savedPerson.toJSON())   
   })
 })
+
+app.put('/api/persons/:id', (request, response,next) => {
+  const body = request.body;
+  const person = {
+    number:body.number
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(updatedPerson => {
+    response.json(updatedPerson.toJSON())
+  }).catch(error => next(error))
+
+})
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler);
 
 
 const PORT = process.env.PORT || 3001
